908054509377a528848c76e58e0ecb7f936c6947
johnpat24 <johnpatrickandaquig10@gmail.com>
2025-02-10 23:38:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//var
+var num = 49
+
+var num = 50
+
+num = 10
+console.log(num)
+
+//let
+let num1 = 49
+
+num1 = 10
+console.log(num1)
+
+//conts
+const num2 = 49
+const num2 = 50
+
+num2 = 10
+console.log(num2)
